7ed99bf29ca80bed8b4edf28986a32974521dc43
ooyeraju <rajnishkush2@gmail.com>
2021-05-14 12:59:58 +0530

    replaced date with numbers and added length of dict

--- app.py
+++ app.py
@@ -7,35 +7,43 @@ from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
+app.config["CORS_HEADERS"] = "Content-Type"
 
-@app.route('/', methods = ['POST'])
+
+@app.route("/", methods=["POST"])
 @cross_origin()
 def solve():
-    context = request.json['context']
-    if request.method == 'POST':
+    context = request.json["context"]
+    if request.method == "POST":
 
-        values = yf.Ticker(context['ticker']).history(start=context['start'],  end=context['end'])
+        values = yf.Ticker(context["ticker"]).history(
+            start=context["start"], end=context["end"]
+        )
 
         res = {}
 
         for i in range(0, values.shape[0]):
 
-            res[values.iloc[i].name.strftime("%Y-%m-%d")] = {
-
-                'Open': values.iloc[i]['Open'], 
-                'High': values.iloc[i]['High'],
-                'Low': values.iloc[i]['Low'],
-                'Close': values.iloc[i]['Close'],
-                'Volume': values.iloc[i]['Volume'],
-                'Dividends': values.iloc[i]['Dividends'],
-                'Stock Splits': values.iloc[i]['Stock Splits'],
-                
-                }
+            res[i] = {
+                "Date": values.iloc[i].name.strftime("%Y-%m-%d"),
+                "Open": values.iloc[i]["Open"],
+                "High": values.iloc[i]["High"],
+                "Low": values.iloc[i]["Low"],
+                "Close": values.iloc[i]["Close"],
+                "Volume": values.iloc[i]["Volume"],
+                "Dividends": values.iloc[i]["Dividends"],
+                "Stock Splits": values.iloc[i]["Stock Splits"],
+            }
 
             print(values.iloc[i])
 
-        return jsonify(res)
+        data = {
+            "length": values.shape[0],
+            "data": res,
+        }
+
+        return jsonify(data)
+
 
 if __name__ == "__main__":
     app.run()
