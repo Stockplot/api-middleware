ee407257cdbe714da64f4b1df3aac4c08ffe3dff
Rahul Gorai <rahul.gorai.3141@gmail.com>
2021-06-10 03:41:35 +0530

    Return portfolio values for BB

--- app.py
+++ app.py
@@ -90,6 +90,13 @@ def BBands():
 
         res = {}
 
+        selling = False
+        ordered_signals = []
+        liquid_amount = int(context["investment"])
+        invested_amount = 0
+        num_shares = 0
+        last_total = liquid_amount
+
         for price in close:
 
             price_sum += price
@@ -111,9 +118,50 @@ def BBands():
                     "lower_band": avg - sdfactor * (sd_sum ** 0.5),
                 }
 
+                if (close[idx - window] <= avg - sdfactor * (sd_sum ** 0.5)) and selling == False:
+                    num_shares = liquid_amount // close[idx - window]
+                    liquid_amount -= num_shares * close[idx - window]
+                    invested_amount += num_shares * close[idx - window]
+                    total_amount = liquid_amount + invested_amount
+                    pnl = total_amount - last_total
+                    last_total = total_amount
+                    ordered_signals.append({
+                        "date": values.iloc[idx - window].name.strftime("%Y-%m-%d"),
+                        "signal": 1,
+                        "price": close[idx - window],
+                        "invested_amount": invested_amount,
+                        "liquid_amount": liquid_amount,
+                        "total_amount": total_amount,
+                        "pnl": pnl,
+                    })
+                    selling = True
+                elif (close[idx - window] >= avg + sdfactor * (sd_sum ** 0.5)) and selling == True:
+                    liquid_amount += num_shares * close[idx - window]
+                    invested_amount = 0
+                    total_amount = liquid_amount + invested_amount
+                    pnl = total_amount - last_total
+                    last_total = total_amount
+                    ordered_signals.append({
+                        "date": values.iloc[idx - window].name.strftime("%Y-%m-%d"),
+                        "signal": -1,
+                        "price": close[idx - window],
+                        "invested_amount": invested_amount,
+                        "liquid_amount": liquid_amount,
+                        "total_amount": total_amount,
+                        "pnl": pnl,
+                    })
+                    selling = False
+
+
+
             idx += 1
 
-        data = {"data": res, "length": idx - window}
+        data = {
+            "data": res, 
+            "data_length": idx - window,
+            "ordered_signals": ordered_signals,
+            "ordered_signals_len": len(ordered_signals),
+            }
 
         return jsonify(data)
 
